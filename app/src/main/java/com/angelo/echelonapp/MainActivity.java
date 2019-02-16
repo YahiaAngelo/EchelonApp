41dbaea0f417d8ac0f7af5417f01690004644162
YahiaAngelo <developer@nserrao.com>
2019-02-16 16:57:00 +0200

    Rebase

--- app/src/main/java/com/angelo/echelonapp/MainActivity.java
+++ app/src/main/java/com/angelo/echelonapp/MainActivity.java
@@ -1,10 +1,11 @@
 package com.angelo.echelonapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Context;
+
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
+
+import com.angelo.echelonapp.fragments.FlasherFragment;
 import com.angelo.echelonapp.fragments.UserFragment;
 import com.mikepenz.materialdrawer.AccountHeader;
 import com.mikepenz.materialdrawer.AccountHeaderBuilder;
@@ -13,7 +14,6 @@ import com.mikepenz.materialdrawer.DrawerBuilder;
 import com.mikepenz.materialdrawer.model.DividerDrawerItem;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
 import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
-import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
 public class MainActivity extends AppCompatActivity {
@@ -35,32 +35,36 @@ public class MainActivity extends AppCompatActivity {
                 .addProfiles(
                         new ProfileDrawerItem().withName(getString(R.string.app_name)).withIcon(getResources().getDrawable(R.mipmap.ic_launcher))
                 ).build();
-        PrimaryDrawerItem item1 = new PrimaryDrawerItem().withIdentifier(1).withName("Echelon Devs.");
 
-        SecondaryDrawerItem item2=new SecondaryDrawerItem().withIdentifier(2).withName("Test for sth ");
-        final Context context=getApplicationContext();
-        final Drawer result = new DrawerBuilder()
+        PrimaryDrawerItem item1 = new PrimaryDrawerItem().withIdentifier(1).withName("Echelon Devs.");
+        PrimaryDrawerItem item2 = new PrimaryDrawerItem().withIdentifier(2).withName("Flash Zips");
+
+        Drawer result = new DrawerBuilder()
                 .withActivity(this)
                 .withToolbar(toolbar)
                 .withAccountHeader(headerResult)
                 .addDrawerItems(
                         item1,
                         item2,
-                       new DividerDrawerItem()
+                        new DividerDrawerItem()
                 )
                 .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
                     @Override
                     public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
-                        long id=drawerItem.getIdentifier();
-                        int id0=Math.toIntExact(id);
-                        switch (id0){
-                            case 1: Toast.makeText(context,"smh on you android",Toast.LENGTH_LONG);
-                                break;
-                            case 2: Toast.makeText(context,"ok android?",Toast.LENGTH_LONG).show();
+                        switch (position){
+                            case 1:
+                                getSupportFragmentManager().beginTransaction()
+                                        .replace(R.id.container, new UserFragment())
+                                        .commit();
                                 break;
 
+                            case 2:
+                                getSupportFragmentManager().beginTransaction()
+                                        .replace(R.id.container, new FlasherFragment())
+                                        .commit();
+
                         }
-                        return false;
+                        return true;
                     }
                 })
                 .build();
